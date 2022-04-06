8dda768d1d922444e43045d565444c0877a15525
Luc Bloom <luc@storygiantgames.com>
2022-04-07 00:50:15 +0200

    Better hit matching with image shape & settings

--- ImageWithHitDetectionOnShape.cs
+++ ImageWithHitDetectionOnShape.cs
@@ -4,19 +4,51 @@ using UnityEngine.UI;
 
 public class ImageWithHitDetectionOnShape : Image
 {
+#if DEVELOP_HIT_DETECTION
+	int m_HitState;
+	Vector2 m_InputScreenPoint;
+	Vector2 m_CalculatedPointInSprite;
+	Camera m_UsedCam;
+#endif // DEVELOP_HIT_DETECTION
+
+	Vector2 GetSizeForCalc()
+	{
+		if (preserveAspect && (type == Image.Type.Simple || type == Image.Type.Filled))
+		{
+			return sprite.rect.size * Mathf.Min(rectTransform.sizeDelta.x / sprite.rect.width, rectTransform.sizeDelta.y / sprite.rect.height);
+		}
+		return rectTransform.sizeDelta;
+	}
+
 	override public bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
 	{
 		var verts = sprite.vertices;
 		var tris = sprite.triangles;
 
-		if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, screenPoint, eventCamera, out var local))
+#if DEVELOP_HIT_DETECTION
+		m_InputScreenPoint = screenPoint;
+#endif // DEVELOP_HIT_DETECTION
+
+		if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, screenPoint, eventCamera, out var localPos))
 		{
+#if DEVELOP_HIT_DETECTION
+			m_HitState = 0; // Outside rect
+#else
 			return false;
+#endif // DEVELOP_HIT_DETECTION
 		}
-		var ext = Vec.XY(sprite.bounds.extents);
-		var ctr = Vec.XY(sprite.bounds.center);
-		var offset = ctr + sprite.textureRectOffset / new Vector2(sprite.texture.width, sprite.texture.height);
-		var pointInSpriteSpace = offset + 2 * ext * local / sprite.rect.size;
+
+		var size = GetSizeForCalc();
+		var pointInSpriteSpace = localPos;
+		pointInSpriteSpace *= sprite.bounds.extents.xy() * 2 / size;
+		pointInSpriteSpace -= Vector2.one;
+		pointInSpriteSpace += rectTransform.pivot * 2;
+		pointInSpriteSpace += sprite.textureRectOffset / Vec.xy(sprite.texture.width, sprite.texture.height);
+		pointInSpriteSpace += sprite.bounds.center.xy();
+
+#if DEVELOP_HIT_DETECTION
+		m_CalculatedPointInSprite = pointInSpriteSpace;
+#endif // DEVELOP_HIT_DETECTION
 		for (var i = 0; i < tris.Length; i += 3)
 		{
 			var a = verts[tris[i + 0]];
@@ -24,12 +56,80 @@ public class ImageWithHitDetectionOnShape : Image
 			var c = verts[tris[i + 2]];
 			if (IsPointInTriangle(pointInSpriteSpace, a, b, c))
 			{
+#if DEVELOP_HIT_DETECTION
+				m_HitState = 2; // Hit
+#endif // DEVELOP_HIT_DETECTION
 				return true;
 			}
 		}
 
+#if DEVELOP_HIT_DETECTION
+		m_HitState = 1; // Outside triangles
+#endif // DEVELOP_HIT_DETECTION
 		return false;
 	}
+
+#if DEVELOP_HIT_DETECTION
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(10.0f, 10.0f, 200.0f, 30.0f), $"Draw Debug ({m_CalculatedPointInSprite})"))
+		{
+		}
+		DrawDebug();
+	}
+
+	void DrawDebug()
+	{
+		var triangles = sprite.triangles;
+		var vertices = sprite.vertices;
+
+		var size = GetSizeForCalc();
+		Vector2 ToScreenPoint(Vector2 p)
+		{
+			p -= sprite.bounds.center.xy();
+			p -= sprite.textureRectOffset / Vec.xy(sprite.texture.width, sprite.texture.height);
+			p -= rectTransform.pivot * 2;
+			p += Vector2.one;
+			p /= sprite.bounds.extents.xy() * 2 / size;
+			p = transform.TransformPoint(p);
+			if (m_UsedCam)
+			{
+				p = m_UsedCam.WorldToScreenPoint(p);
+			}
+			return p;
+		};
+
+		// Draw the triangles using grabbed vertices
+		for (int i = 0; i < triangles.Length; i += 3)
+		{
+			var list = new[] {
+				vertices[triangles[i + 0]],
+				vertices[triangles[i + 1]],
+				vertices[triangles[i + 2]],
+			};
+	
+			var color = IsPointInTriangle(m_CalculatedPointInSprite, list[0], list[1], list[2]) ? Color.green : Color.red;
+
+			// To see these you must view the game in the Scene tab while in Play mode
+			list.For((i, p) => list[i] = ToScreenPoint(p));
+			list.For((i, p) => Debug.DrawLine(p, list.Mod(i+1), color, 0.2f));
+		}
+	
+		if (m_CalculatedPointInSprite != Vector2.zero)
+		{
+			var color = m_HitState == 2 ? Color.blue : (m_HitState == 1 ? Color.yellow : Color.gray);
+			var screenPt = ToScreenPoint(m_CalculatedPointInSprite);
+			Debug.DrawLine(screenPt + new Vector2(-10, 10), screenPt + new Vector2(10, -10), color, 0.2f);
+			Debug.DrawLine(screenPt + new Vector2(-10, -10), screenPt + new Vector2(10, 10), color, 0.2f);
+		}
+
+		if (m_InputScreenPoint != Vector2.zero)
+		{
+			Debug.DrawLine(m_InputScreenPoint + new Vector2(-5, 0), m_InputScreenPoint + new Vector2(5, 0), Color.cyan, 0.2f);
+			Debug.DrawLine(m_InputScreenPoint + new Vector2(0, -5), m_InputScreenPoint + new Vector2(0, 5), Color.cyan, 0.2f);
+		}
+	}
+#endif // DEVELOP_HIT_DETECTION
 
 	bool IsPointInTriangle(Vector2 p, Vector2 a, Vector2 b, Vector2 c)
 	{
--- README.md
+++ README.md
@@ -8,8 +8,22 @@ Usage:
 - On the Button, remove the Image component.
 - Add an ImageWithHitDetectionOnShape component.
 - Don't forget to patch up the &lt;Missing Image> link the in the Button component again.
+- Check "Use Sprite Shape" on the image if you haven't already.
 
 The button will use Unity's sprite shape by default. If that's not fitting very well, or you want a custom one (i.e. no holes in the middle, padding):
 - Go to the texture's properties and use the Shape Editor do draw the clickable area.
 
 Because this component uses hit-testing with triangles on the button, you could extend it with functionality to test other shapes as well.
+
+Features:
+- Matches Image on height, scale, rotation, free-form width & height
+- Matches Image when "Preserve Aspect Ratio" is checked
+- Matches Image when "Simple" or "Filled" is selected
+- Commented out debug code to visually see how (and if) it works
+
+Limitations:
+- Doesn't match exactly when image type "Sliced" is selected
+- Doesn't match when image type "Repeat" is selected
+
+Future:
+- If someone can tell me how to get the triangle mesh for an image that has type "Slice", that would be great.
